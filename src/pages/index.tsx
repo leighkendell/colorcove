cb248d6dafa6aa91fb7790a9111fc8123b43a61c
Leigh Kendell <leighkendell@gmail.com>
2019-05-29 22:10:40 +0800

    Add some initial home page content blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Header from '../components/header';
+import TextBlock from '../components/text-block';
+import Section from '../components/section';
+import Wrapper from '../components/wrapper';
 
 const IndexPage = ({ data: { prismicHome } }: any) => {
   return (
@@ -10,6 +13,28 @@ const IndexPage = ({ data: { prismicHome } }: any) => {
         description={prismicHome.data.intro.text}
         image={prismicHome.data.image.localFile.childImageSharp.fluid}
       />
+
+      {prismicHome.data.body.map(({ slice_type, primary }: any) => {
+        console.log(slice_type, primary);
+        let content;
+
+        // Feature text block
+        if (slice_type === 'feature_text') {
+          content = (
+            <TextBlock
+              title={primary.heading.text}
+              description={primary.text.text}
+              horizontal={primary.layout === 'Horizontal'}
+            />
+          );
+        }
+
+        return (
+          <Section>
+            <Wrapper>{content}</Wrapper>
+          </Section>
+        );
+      })}
     </>
   );
 };
@@ -31,6 +56,21 @@ export const pageQuery = graphql`
             childImageSharp {
               fluid(maxWidth: 2880) {
                 ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        body {
+          slice_type
+          ... on PrismicHomeBodyFeatureText {
+            id
+            primary {
+              layout
+              heading {
+                text
+              }
+              text {
+                text
               }
             }
           }
