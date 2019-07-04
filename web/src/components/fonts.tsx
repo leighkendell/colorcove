501b53a29ebdb3d2c14df3cf89860ba4ff9b1858
Leigh Kendell <leighkendell@gmail.com>
2019-07-04 21:43:21 +0800

    Update font loading strategy

--- web/src/components/fonts.tsx
+++ web/src/components/fonts.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import '../fonts/fonts.css';
 
 // Fonts
 import ManropeLightWoff2 from '../fonts/Manrope3-Light.woff2';
 import ManropeBoldWoff2 from '../fonts/Manrope3-Bold.woff2';
+import ManropeLightWoff from '../fonts/Manrope3-Light.woff';
+import ManropeBoldWoff from '../fonts/Manrope3-Bold.woff';
 
 const Fonts: React.FC = () => {
   return (
@@ -23,6 +24,24 @@ const Fonts: React.FC = () => {
         type="font/woff2"
         crossOrigin="anonymous"
       ></link>
+      <style type="text/css">{`
+        @font-face {
+          font-weight: 300;
+          font-family: 'Manrope';
+          font-style: normal;
+          src: url(${ManropeLightWoff2}) format('woff2'),
+            url(${ManropeLightWoff}) format('woff');
+          font-display: swap;
+        }
+        @font-face {
+          font-weight: bold;
+          font-family: 'Manrope';
+          font-style: normal;
+          src: url(${ManropeBoldWoff2}) format('woff2'),
+            url(${ManropeBoldWoff}) format('woff');
+          font-display: swap;
+        }
+      `}</style>
     </Helmet>
   );
 };
