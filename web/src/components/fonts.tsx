1a3c16a059e0493d85bbd0619129e60f2d5925c9
Leigh Kendell <leighkendell@gmail.com>
2019-07-06 15:16:24 +0800

    Update deps and font loading

--- web/src/components/fonts.tsx
+++ web/src/components/fonts.tsx
@@ -2,46 +2,69 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 // Fonts
-import ManropeLightWoff2 from '../fonts/Manrope3-Light.woff2';
-import ManropeBoldWoff2 from '../fonts/Manrope3-Bold.woff2';
-import ManropeLightWoff from '../fonts/Manrope3-Light.woff';
-import ManropeBoldWoff from '../fonts/Manrope3-Bold.woff';
+import ManropeStage1LightWoff2 from '../fonts/manrope-light-kern-latin.woff2';
+import ManropeStage1LightWoff from '../fonts/manrope-light-kern-latin.woff';
+import ManropeStage1BoldWoff2 from '../fonts/manrope-bold-kern-latin.woff2';
+import ManropeStage1BoldWoff from '../fonts/manrope-bold-kern-latin.woff';
+import ManropeLightWoff2 from '../fonts/manrope-light-hint-all.woff2';
+import ManropeLightWoff from '../fonts/manrope-light-hint-all.woff';
+import ManropeBoldWoff2 from '../fonts/manrope-bold-hint-all.woff2';
+import ManropeBoldWoff from '../fonts/manrope-bold-hint-all.woff';
 
 const Fonts: React.FC = () => {
   return (
     <Helmet>
       <link
         rel="preload"
-        href={ManropeLightWoff2}
+        href={ManropeStage1LightWoff2}
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       ></link>
       <link
         rel="preload"
-        href={ManropeBoldWoff2}
+        href={ManropeStage1BoldWoff2}
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       ></link>
-      <style type="text/css">{`
+      <style>{`
         @font-face {
           font-weight: 300;
           font-family: 'Manrope';
           font-style: normal;
-          src: url(${ManropeLightWoff2}) format('woff2'),
-            url(${ManropeLightWoff}) format('woff');
+          src: url(${ManropeStage1LightWoff2}) format('woff2'),
+            url(${ManropeStage1LightWoff}) format('woff');
           font-display: swap;
         }
         @font-face {
           font-weight: bold;
           font-family: 'Manrope';
           font-style: normal;
-          src: url(${ManropeBoldWoff2}) format('woff2'),
-            url(${ManropeBoldWoff}) format('woff');
+          src: url(${ManropeStage1BoldWoff2}) format('woff2'),
+            url(${ManropeStage1BoldWoff}) format('woff');
           font-display: swap;
         }
       `}</style>
+      <script>{`
+        if ('fonts' in document) {
+          const light = new FontFace(
+            'Manrope',
+            "url(${ManropeLightWoff2}) format('woff2'), url(${ManropeLightWoff}) format('woff')",
+            { weight: '300' }
+          );
+          const bold = new FontFace(
+            'Manrope',
+            "url(${ManropeBoldWoff2}) format('woff2'), url(${ManropeBoldWoff}) format('woff')",
+            { weight: 'bold' }
+          );
+          Promise.all([bold.load(), light.load()]).then(function(fonts) {
+            fonts.forEach(function(font) {
+              document.fonts.add(font);
+            });
+          });
+        }
+      `}</script>
     </Helmet>
   );
 };
