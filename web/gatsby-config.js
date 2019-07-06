7323ba1d89a93595a6e78439bb832b75fd07ebb3
Leigh Kendell <leighkendell@gmail.com>
2019-07-06 16:25:44 +0800

    Downgrade shopify buy, enable gatsby-plugin-offline

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -84,8 +84,6 @@ module.exports = {
         accessToken: `${process.env.GATSBY_SHOPIFY_STOREFRONT_TOKEN}`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
