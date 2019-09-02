582c8a6f4deb8082041042cbd89f999126729cee
Leigh Kendell <leighkendell@gmail.com>
2019-09-02 11:50:35 +0800

    Increase image quality for webp images

--- web/src/components/image.tsx
+++ web/src/components/image.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Img, { FluidObject } from 'gatsby-image';
 import { colorcoveTheme } from '../utils/theme';
 
@@ -12,18 +12,43 @@ interface Props {
 }
 
 const Image: React.FC<Props> = React.forwardRef<Img, Props>(
-  ({ image, alt, onLoad, backgroundColor, className, style }, ref) => (
-    <Img
-      fluid={image}
-      backgroundColor={backgroundColor || colorcoveTheme.colorLightGrey}
-      fadeIn={true}
-      onLoad={onLoad}
-      alt={alt}
-      className={className}
-      style={style}
-      ref={ref}
-    />
-  )
+  ({ image, alt, onLoad, backgroundColor, className, style }, ref) => {
+    const [fluidObject, setFluidObject] = useState<FluidObject | null>(null);
+
+    // TODO: Replace this when sanity has a proper way of specifying image quality params
+    // Sets the fluid object based on props and replaces all webp sources with heigher quality output
+    useEffect(() => {
+      const replaceSrc = (imageString: string) =>
+        imageString.replace(/&fm=webp/g, '$&&q=95');
+
+      if (image.srcWebp && image.srcSetWebp) {
+        const srcWebp = replaceSrc(image.srcWebp);
+        const srcSetWebp = replaceSrc(image.srcSetWebp);
+        setFluidObject({
+          ...image,
+          srcWebp,
+          srcSetWebp,
+        });
+      }
+    }, [image]);
+
+    return (
+      <>
+        {fluidObject && (
+          <Img
+            fluid={fluidObject}
+            backgroundColor={backgroundColor || colorcoveTheme.colorLightGrey}
+            fadeIn={true}
+            onLoad={onLoad}
+            alt={alt}
+            className={className}
+            style={style}
+            ref={ref}
+          />
+        )}
+      </>
+    );
+  }
 );
 
 export default Image;
