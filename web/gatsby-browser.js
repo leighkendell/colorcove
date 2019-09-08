24aa798e6e8b75637ec5dc83b1f507148fa37dcc
Leigh Kendell <leighkendell@gmail.com>
2019-09-08 17:12:10 +0800

    Add intersection observer polyfill

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+async function loadPolyfills() {
+  if (typeof window.IntersectionObserver === 'undefined') {
+    await import('intersection-observer');
+  }
+}
+
+exports.onClientEntry = () => {
+  loadPolyfills();
+};
