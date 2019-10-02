bb9e544148d6d6ae2a00f39293cc61097e8e4057
Leigh Kendell <leighkendell@gmail.com>
2019-10-02 22:59:57 +0800

    Update deps, remove logrocket

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,7 +1,6 @@
 require('intersection-observer');
 const smoothscroll = require('smoothscroll-polyfill');
 const Sentry = require('@sentry/browser');
-const LogRocket = require('logrocket');
 
 function loadPolyfills() {
   smoothscroll.polyfill();
@@ -9,12 +8,6 @@ function loadPolyfills() {
 
 function loadTracking() {
   Sentry.init({ dsn: process.env.GATSBY_SENTRY_DSN });
-  LogRocket.init(process.env.GATSBY_LOGROCKET_KEY);
-  LogRocket.getSessionURL(sessionURL => {
-    Sentry.configureScope(scope => {
-      scope.setExtra('sessionURL', sessionURL);
-    });
-  });
 }
 
 exports.onClientEntry = () => {
