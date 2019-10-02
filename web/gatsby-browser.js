92a9f86b4639f9ab66e3f27d37ac721e33c7a05e
Leigh Kendell <leighkendell@gmail.com>
2019-10-02 23:22:58 +0800

    Ad FB pixel tracking code

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -14,3 +14,11 @@ exports.onClientEntry = () => {
   loadPolyfills();
   loadTracking();
 };
+
+// Track FB pixel page view
+exports.onRouteUpdate = () => {
+  if (process.env.NODE_ENV !== `production` || typeof fbq !== `function`) {
+    return null;
+  }
+  window.fbq('track', 'ViewContent');
+};
--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
 
-// You can delete this file if you're not using it
+// Load FB Pixel script
+export const onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.NODE_ENV !== `production`) {
+    return null;
+  }
+
+  setHeadComponents([
+    <script
+      key="fb-pixel-script"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', ${process.env.FB_PIXEL_ID});
+          fbq('track', 'PageView');
+        `,
+      }}
+    ></script>,
+    <noscript
+      key="fb-pixel-noscript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          <img height="1" width="1"
+          src="https://www.facebook.com/tr?id=${process.env.FB_PIXEL_ID}&ev=PageView
+          &noscript=1"/>
+        `,
+      }}
+    ></noscript>,
+  ]);
+};
